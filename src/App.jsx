a7522144aa77b64d462610594e5fa1a6bc4fcccf
Kevin <wudkevin@icloud.com>
2024-05-30 19:40:16 -0400

    I created a new portfolio project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import Layout from "./pages/Layout"
 
 function App() {
   
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <Layout />
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../pages/Layout'
+
+function Home() {
+    return (
+        <Layout>
+            <div>
+                Home page
+            </div>
+        </Layout>
+
+    )
+}
+
+export default Home
