ca05f588cd3266b402c2e7e733ff27cc24ba3d4d
Kevin <wudkevin@icloud.com>
2024-05-30 22:49:42 -0400

    I create the components which contain the navigation part in which we have the logo and others. For the logo i had also add react logo that i install in the terminal

---
+++ src/components/nav.jsx
@@ -0,0 +1,23 @@
+
+
+const nav = () => {
+  return (
+   <>
+     <div className="flex items-center justify-between p-10 lg:flex-row
+     ">
+        <div>
+            <a href="#" className="text-white font-bold text-3xl tracking-wider flex items-center">
+                PORTOFOLIO</a>
+        </div>
+        <div>
+            <a href="#">Skills</a>
+            <a href="#">Projects</a>
+            <a href="#">Testimonials</a>
+        </div>
+        <div></div>
+     </div>
+   </>
+  )
+}
+
+export default nav
