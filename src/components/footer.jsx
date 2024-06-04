01fbe19d8edd1ad7523da8dfe5843d7fa3df1681
Kevin <wudkevin@icloud.com>
2024-06-04 18:00:28 -0400

    replace nae

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function footer() {
   return (
     <div className="flex justify-center bg-black text-white py-5">
-    Copyright &copy; 2024 Wud_kvn
+    Copyright &copy; 2024 wud-kvn
       </div>
   )
 }
